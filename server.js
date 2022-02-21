18c42e4c96f2966102dbbf54f4151bfae2fe5311
Jacob <jacob.tobin.liberty@gmail.com>
2022-02-21 03:29:40 -0500

    login/logout/signup functionality added to front-end and integrated to back end

--- server.js
+++ server.js
@@ -1,14 +1,19 @@
 const path = require('path');
 const express = require('express');
 const exphbs = require('express-handlebars');
-
 const app = express();
 const PORT = process.env.PORT || 3001;
-
 const sequelize = require('./config/connection');
-
 const hbs = exphbs.create({});
+const session = require('express-session');
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+};
 
+app.use(session(sess));
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
